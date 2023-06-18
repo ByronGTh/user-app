038a89afb1b0624f96e29b0ced53c541786bcc8f
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-06-18 15:51:37 -0600

    main.jsx: agregando componente principal al archivo principal del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { UsersApp } from './UsersApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UsersApp />
   </React.StrictMode>,
 )
