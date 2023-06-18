e522e9ab6d8bffe483f79d5eff6a5054b34d8de9
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-06-18 15:53:36 -0600

    src/UsersApp.jsx: Agregando componentes UsersList y UserForm al componente principal

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,9 +1,16 @@
+import { UserForm } from "./components/UserForm";
+import { UsersList } from "./components/UsersList";
+
 export const UsersApp = () =>{
     return(<div className="container my-4">
         <h2>User App</h2>
         <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
+            <div className="col">
+                <UsersList/>
+            </div>
+            <div className="col">
+                <UserForm/>
+            </div>
         </div>
     </div>);
 }
