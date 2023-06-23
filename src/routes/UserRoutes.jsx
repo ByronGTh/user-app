694f2fc99cf04fd5f2721fa6c4f0dd57e07290f9
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-06-22 20:37:21 -0600

    main.jsx: llamada a componente principal dentro de las etiquetas BrowserRouter, UserRoutes.jsx: manejo de rutas, UserApp.jsx: control de las rutas de acceso privado y las rutas publicas

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,4 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { UsersPage } from "../pages/UsersPage";
+import { Navbar } from "../components/layout/Navbar";
 
 export const UserRoutes =({login, handlerLogout})=>{
     return(<>
