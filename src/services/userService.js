149ae79cb4651ede933dd1fbd5bfa8f36be85d8e
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-07-18 17:30:09 -0600

    UserService.js: Cambiando la constante config por una funcion para evitar que converve valores de secciones anteriores

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,16 @@
 import axios from "axios"
 
 const URL_BACKEND = "http://localhost:8080/users";
+
+const config = () => {
+   return {
+        headers:{
+            "Authorization": sessionStorage.getItem('token'),
+            "Content-Type": "application/json"
+        }
+    }
+}
+
 export const findAll = async () =>{
     try {
         const response = await axios.get(URL_BACKEND);
@@ -13,7 +23,7 @@ export const findAll = async () =>{
 
 export const save = async ({ username, email, password }) => {
     try {
-        return await axios.post(URL_BACKEND, {username, email, password});
+        return await axios.post(URL_BACKEND, {username, email, password}, config());
     } catch (error) {
         throw error; //lanza o envia el error al useUsers
         //console.error(error);
@@ -22,7 +32,7 @@ export const save = async ({ username, email, password }) => {
 
 export const update = async ({ id, username, email }) => {
     try {
-        return await axios.put(`${URL_BACKEND}/${id}`, { username, email, password: 'vacio' });
+        return await axios.put(`${URL_BACKEND}/${id}`, { username, email, password: 'vacio' }, config());
     } catch (error) {
         throw error;
         //console.error(error);
@@ -31,7 +41,7 @@ export const update = async ({ id, username, email }) => {
 
 export const remove = async (id) => {
     try {
-        await axios.delete(`${URL_BACKEND}/${id}`);
+        await axios.delete(`${URL_BACKEND}/${id}`, config());
     } catch (error) {
         throw error;
         //console.error(error);
