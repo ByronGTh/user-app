8cb23a5ea3736c44f7ce81bd664dc5e3fd7b00c0
Byron Gonzalez <ing.byrongonzalez@gmail.com>
2023-07-20 06:39:41 -0600

    src/AppRoutes.jsx: creando el archivo de rutas para este contenido de codigo del UsersApp.jsx

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,26 +1,9 @@
-import { LoginPage } from "./auth/pages/LoginPage";
-import { UsersPage } from "./pages/UsersPage";
-import { Navbar } from "./components/layout/Navbar";
-import { useAuth } from "./auth/hooks/useAuth";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { UserRoutes } from "./routes/UserRoutes";
+import { AppRoutes } from "./AppRoutes"
+
 
 export const UsersApp = () => {
 
-    const { login, handlerLogin, handlerLogout } = useAuth();
-
-    return (
-        <Routes>
-            {
-                login.isAuth
-                    ? (
-                        <Route path="/*" element={<UserRoutes login={login} handlerLogout={handlerLogout}/>}/>
-                    )
-                    : <>
-                        <Route path="/login" element={<LoginPage handlerLogin={handlerLogin} />}/>
-                        <Route path="/*" element={ <Navigate to="/login"/> }/>
-                    </>
-            }
-        </Routes>
-    );
+    return(
+        <AppRoutes/>
+    )
 }
